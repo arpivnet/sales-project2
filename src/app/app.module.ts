e0037efca72ad67bf74c18c7ed6c1e76ecbf9e3c
arpivnet <arpivnet@gmail.com>
2022-04-11 20:57:43 -0400

    added images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
+import { ProductListComponent } from './componenets/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services_folder/product.service';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    SalesPersonListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule
+  ({
+    declarations: [AppComponent, ProductListComponent],
+
+    imports: [BrowserModule, HttpClientModule],
+
+    providers: [ProductService],
+
+    bootstrap: [AppComponent],
+  })
+
 export class AppModule { }
